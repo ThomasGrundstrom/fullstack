41ead2e331e74e0133a63eb08778dc638a17a67f
Thomas Grundström <thomas.grundstrom@helsinki.fi>
2024-06-23 19:52:02 +0300

    Tila tehtävän 4.20 jälkeen

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -10,6 +10,18 @@ mongoose.connect(config.MONGODB_URI)
 app.use(cors())
 app.use(express.json())
 
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    const token = authorization.replace('Bearer ', '')
+    request.token = token
+  } else {
+    request.token = null
+  }
+  next()
+}
+app.use(tokenExtractor)
+
 const blogsRouter = require('./controllers/blogs')
 app.use('/api/blogs', blogsRouter)
 const usersRouter = require('./controllers/users')
@@ -21,6 +33,8 @@ const errorHandler = (error, request, response, next) => {
   console.error(error.message)
   if (error.name === 'MongoServerError' && error.message.includes('E11000 duplicate key error')) {
     return response.status(400).json({ error: 'username already in use' })
+  } else if (error.name === 'JsonWebTokenError') {
+    return response.status(401).json({ error: 'token missing or invalid' })
   }
   next(error)
 }
--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -2,6 +2,7 @@ const blogsRouter = require('express').Router()
 const { response } = require('../app')
 const Blog = require('../models/blog')
 const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({}).populate('user', { username: 1, name: 1, id: 1})
@@ -10,8 +11,12 @@ blogsRouter.get('/', async (request, response) => {
 
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
-  const users = await User.find({})
-  const user = users[0]
+
+  const decodedToken = jwt.verify(request.token, process.env.SECRET)
+  if (!decodedToken.id) {
+    return response.status(401).json({ error: 'invalid token' })
+  }
+  const user = await User.findById(decodedToken.id)
 
   const blog = new Blog({
     title: body.title,
@@ -27,7 +32,6 @@ blogsRouter.post('/', async (request, response) => {
     const savedBlog = await blog.save()
     user.blogs = user.blogs.concat(savedBlog._id)
     await user.save()
-    
     response.status(201).json(savedBlog)
   }
 })
