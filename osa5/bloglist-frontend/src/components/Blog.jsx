fcdfed2fe81a7763ac08a9d2aca215f81c2d8e83
Thomas Grundström <thomas.grundstrom@helsinki.fi>
2024-06-26 13:16:46 +0300

    tila tehtävän 5.7 jälkeen

--- osa5/bloglist-frontend/src/components/Blog.jsx
+++ osa5/bloglist-frontend/src/components/Blog.jsx
@@ -1,7 +1,30 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from "react"
+
+const Blog = ({ blog }) => {
+  const [visible, setVisible] = useState(false)
+
+  const hideWhenVisible = { display: visible ? 'none' : '' }
+  const showWhenVisible = { display: visible ? '' : 'none' }
+
+  const toggleVisibility = () => {
+    setVisible(!visible)
+  }
+
+  return (
+    <div>
+      <div style={hideWhenVisible}>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>view</button>
+      </div>
+      <div style={showWhenVisible}>
+        {blog.title} {blog.author}
+        <button onClick={toggleVisibility}>hide</button> <br />
+        {blog.url} <br />
+        likes: {blog.likes} <br />
+        {blog.user.name}
+      </div>
+    </div>
+  )
+}
 
 export default Blog
